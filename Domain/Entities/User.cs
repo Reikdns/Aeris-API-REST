058c5c48ca5d4d6b328bc4a94ccb7de74cf38664
Kevin Pontón <kvnpntn@gmail.com>
2022-06-24 15:52:06 -0500

    First Commit

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Entity;
+public class User
+{
+    public string Nombres { get; set; }
+
+    public string Apellidos { get; set; }
+
+    public int Edad { get; set; }
+
+    public string Username { get; set; } 
+
+    public string Password { get; set; }
+
+    public string Rol { get; set;} 
+
+    public string Identificacion { get; set; }
+
+    public int UserId { get; set; }
+
+}
