03cb52bf8dd4cefd3c7ed9d32f93a2ca95c3723c
Rei <rdurann@sonda.com>
2024-10-01 20:44:33 -0500

    Initial Caché

--- Entity/obj/Debug/net6.0/Entity.AssemblyInfo.cs
+++ Entity/obj/Debug/net6.0/Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afadb34a6ae0cfe7a87339bda44403139d003517")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
